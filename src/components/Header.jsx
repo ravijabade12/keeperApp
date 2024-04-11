d6e331e67bfea0359d52b5966e548f69d07a80c4
ravijabade12 <ravisjabade12@gmail.com>
2024-04-11 14:05:10 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormatListNumberedRtlIcon from '@mui/icons-material/FormatListNumberedRtl';
+
+function Header() {
+  return (
+    <header>
+      <h1><FormatListNumberedRtlIcon/>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
